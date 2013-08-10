4d276baf5dbb42b4f3e790083a0ef37fa12b9b27
Ryan Griffin <ryangrif@gmail.com>
2013-08-09 20:34:39 -0400

    Creating the UI

--- Maya/Startup.py
+++ Maya/Startup.py
@@ -6,18 +6,18 @@ cmds.currentUnit( time='ntsc' )
 
 def createMenu():
 	# Query the names of all "MayaWindows"
-	mi = mc.window('MayaWindow', ma=True, q=True)
+	mi = cmds.window('MayaWindow', ma=True, q=True)
 	 
 	for m in mi:
 		print m
 		# If a name matches 'UserScripts', delete it
 		if m == 'DojoTools':
-			mc.deleteUI('DojoTools', m=True)
+			cmds.deleteUI('DojoTools', m=True)
 		# Create the 'UserScripts' menu. 
-	mc.menu('DojoTools', label='DojoTools', to=True, p="MayaWindow")
+	cmds.menu('DojoTools', label='DojoTools', to=True, p="MayaWindow")
 
 	# Create a menu item for the RDojo UI
-	mc.menuItem('DojoTools', label='RD_UI', c=createLytItem)
+	cmds.menuItem('DojoTools', label='RD_UI', c=createLytItem)
 
 
 def createLytItem(*args):
--- Maya/System/RDojo_UI.py
+++ Maya/System/RDojo_UI.py
@@ -2,6 +2,31 @@ import maya.cmds as cmds
 
 class Rdojo_UI:
 
-	def __init__(self):
-		print "In Rdojo_UI"
+    def __init__(self):
+        print "In Rdojo_UI2"
+
+        self.UI_Elements = {}
+
+        self.windowName = "RdojoUI_Window"
+        if cmds.window(self.windowName, exists=True):
+            cmds.deleteUI(self.windowName)
+
+
+        windowWidth = 80
+        windowHeight = 40
+
+        buttonWidth = 70
+        buttonHeight = 33
+
+        self.UI_Elements["window"] = cmds.window(self.windowName, width=windowWidth, height=windowHeight, title="RDojo_UI", sizeable=True)
+
+        self.UI_Elements["buttonLyt"] = cmds.flowLayout(v=True, width=windowWidth, height=windowHeight)
+        self.UI_Elements["hingeButton"] = cmds.button(label='Hinge_Lyt', width=buttonWidth, height=buttonHeight, p=self.UI_Elements['buttonLyt'], c=self.doStuff)
+
+        cmds.showWindow(self.UI_Elements["window"])
+
+    def doStuff(self, *args):
+        cmds.button(self.UI_Elements["hingeButton"], edit=True, en=False)
+
+
 
